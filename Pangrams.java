a30fb3b21c29a8374699122f334c51e9c6b1bafb
Rohan Roy <43656700+Rohan-Roy@users.noreply.github.com>
2019-02-02 18:44:36 -0800

    Update and rename Pangrams to Pangrams.java

--- Pangrams.java
+++ Pangrams.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Pangrams {
 
     // Complete the pangrams function below.
     static String pangrams(String s) {
